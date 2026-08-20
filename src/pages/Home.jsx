0f2a30819b61d9b9ef64ade1a8c5f053e2624171
Patricia Lyca Castro <patricialycacastro@gmail.com>
2026-08-20 10:52:21 +0800

    change subtext to Names

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,35 +15,23 @@ export default function Home() {
       </header>
 
       <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto justify-around gap-5 py-4 px-8 md:py-10 md:px-6">
-        {/* <Card
-          title="Login Authentication"
-          subtitle="Validate a username and password against sample credentials and manage login/logout state"
-          page={"/login"}
-        />
-        
-        <Card
-          title="Student Grade Evaluation"
-          subtitle="Enter a student's score and get an automatic remark based on grade ranges."
-           page={"/gradeEval"}
-        />
-        */}
         <Card
           num={3}
           title="Password Strength Checker"
-          subtitle="Check password length and receive live feedback on how strong it is."
+          subtitle="Made by John Rafael Rodis"
           page={"/passChecker"}
         />
         <Card
           num={4}
           title="Electricity Bill Calculator"
-          subtitle="Calculate a customer's electricity bill based on kWh consumption and tiered rates."
+          subtitle="Made by Darryl Adrian Salamera"
           page={"/electricity"}
         />
 
         <Card
           num={5}
           title="Employee Attendance Tracker"
-          subtitle="Check an employee's time-in and determine whether they are on time, late, or very late."
+          subtitle="Made by Patricia Lyca Castro"
           page={"/attendance"}
         />
       </main>
